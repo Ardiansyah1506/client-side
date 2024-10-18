b376aa96bd93232ba9862243928b40d280f83677
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-10-18 08:05:28 +0700

    update login sign-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css'  
+import Login from './component/layout/AuthLayout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Belajar Deploy React Menggunakan Vercel</h1>
+      <Login/>
     </>
   )
 }
